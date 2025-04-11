90ff0f149425929d53e7638063d939ddd33febd2
ashevchenko0309 <ashevchenko0309@gmail.com>
2025-04-11 20:26:32 +0300

    pass allowMobileScroll

--- lib/GridItem.jsx
+++ lib/GridItem.jsx
@@ -364,6 +364,7 @@ export default class GridItem extends React.Component<Props, State> {
         onDrag={this.onDrag}
         onStop={this.onDragStop}
         handle={this.props.handle}
+        allowMobileScroll={true}
         cancel={
           ".react-resizable-handle" +
           (this.props.cancel ? "," + this.props.cancel : "")
